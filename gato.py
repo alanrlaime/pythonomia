280c59f7a4161b05ef5a35b7abd593f93e7fb391
AlanR <133004241+alanrlaime@users.noreply.github.com>
2024-12-20 22:49:28 -0400

    Update gato.py

--- gato.py
+++ gato.py
@@ -1,18 +1,5 @@
 def suma(a,b):
   return a+b
-
-a = int(input("n1: "))
-b = int(input("n2: "))
-print("La suma de",a,b,"es",suma(a,b))
-
-def raiz(a):
-  return a**(1/2)
-
-print("operacion de radical")
-
-def ultimo-digito(a):
-  return a%10
-
 #Recursividad
 
 def factoria(n):
